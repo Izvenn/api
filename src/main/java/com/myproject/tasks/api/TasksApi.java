d6c4b0d6920eaab384ded3a5ab1a0afdc4e739d6
Vinicius Galli <seu_email@example.com>
2025-11-15 23:31:43 -0300

    Subindo services e controllers.

---
+++ src/main/java/com/myproject/tasks/api/TasksApi.java
@@ -0,0 +1,61 @@
+package com.myproject.tasks.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.myproject.tasks.dto.TasksDTO;
+import com.myproject.tasks.facade.TaskFacade;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
+@Controller
+@RequestMapping(value = "/tarefas", produces = MediaType.APPLICATION_JSON_VALUE)
+public class TasksApi {
+    @Autowired
+
+    private TaskFacade TaskFacade;
+
+    @PostMapping
+    @ResponseBody
+     public TasksDTO create(@RequestBody TasksDTO tasksDTO){
+        return TaskFacade.create(tasksDTO);
+
+    }
+
+    @PutMapping("/{taskId}")
+    @ResponseBody
+    public TasksDTO update(@PathVariable("taskId") Long taskId,@RequestBody TasksDTO tasksDTO){
+        return TaskFacade.update(tasksDTO,taskId);
+
+    }
+
+    @GetMapping
+    @ResponseBody
+    public List<TasksDTO> getAll(){
+        return TaskFacade.getAll();
+    }
+
+    @DeleteMapping("/taskId")
+    @ResponseBody
+    public String delete(@PathVariable("taskId") Long taskId){
+
+        return TaskFacade.delete(taskId);
+
+    }
+
+    
+    
+ 
+}
--- src/main/java/com/myproject/tasks/facade/TaskFacade.java
+++ src/main/java/com/myproject/tasks/facade/TaskFacade.java
@@ -1,16 +1,46 @@
 package com.myproject.tasks.facade;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import com.myproject.tasks.dto.TasksDTO;
 
-
 @Service
 public class TaskFacade {
-    private static final  Map<Long, TasksDTO> tasks = new HashMap<>();
+    private static final Map<Long, TasksDTO> tasks = new HashMap<>();
 
-    
+    public TasksDTO create(TasksDTO tasksDTO) {
+
+        Long nextId = tasks.keySet().size() + 1L;
+        tasksDTO.setId(nextId);
+        tasks.put(nextId, tasksDTO);
+        return tasksDTO;
+
+    }
+
+    public TasksDTO update(TasksDTO tasksDTO, Long taskId) {
+
+        tasks.put(taskId, tasksDTO);
+        return tasksDTO;
+
+    }
+
+    public TasksDTO getById(Long tarefaId) {
+        return tasks.get(tarefaId);
+    }
+
+    public List<TasksDTO> getAll() {
+        return new ArrayList<>(tasks.values());
+    }
+
+public String delete(Long taskId) {
+    tasks.remove(taskId);
+    return "Deleted";
+}
+
+
 }
